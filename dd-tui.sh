0d00ac780466e9208c3580e1c162865a19041e00
Daniel <235312011+fischblob-lol@users.noreply.github.com>
2026-05-23 12:47:59 +0200

    add initial placeholder dd-tui.sh

---
+++ dd-tui.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# warning:
+# there are no releases yet
+# this is just a placeholder for when the actual thing comes
+# - daniel (or fischblob-lol)
+
+echo "#########################################"
+echo "#                DD-TUI                 #"
+echo "#########################################"
+echo "a simple, easy to use, fast TUI for dd written in bash"
+echo "there are no releases yet and this is just a placeholder"
